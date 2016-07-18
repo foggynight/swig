229aaff9a2c93722db168f106a7e06ced9462805
Gerald Wodni <gerald.wodni@gee.at>
2016-07-19 00:50:48 +0200

    Forth test-cases: common test-header started

---
+++ Examples/test-suite/forth/swig_testcases.h
@@ -0,0 +1,8 @@
+/* some fixes to compile the test-cases products (.4thx) */
+
+/* assumed to be included by some examples */
+#include <stdlib.h>
+
+/* extern_declaration.i */
+#define SWIGEXPORT
+#define SWIGSTDCALL
